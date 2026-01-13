38948e1a3782ba96add9e97edeb82c0e17dc37e7
petap0w <cks85@msn.com>
2026-01-13 13:41:10 +0100

    Set tutorial default to false

--- src/store/useStorage.js
+++ src/store/useStorage.js
@@ -137,7 +137,7 @@ export const useStorage = create(
       icons: {},
       audio: {},
       menus: {},
-      tutorial: true,
+      tutorial: false,
       sidebar: '',
       scanAreasMenu: '',
       advMenu: {
