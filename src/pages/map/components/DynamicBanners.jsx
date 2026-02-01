f71271b7a57e75d650789d79ca29c53d76000e52
petap0w <cks85@msn.com>
2026-02-01 23:58:27 +0100

    Adjust banner styling

--- src/pages/map/components/DynamicBanners.jsx
+++ src/pages/map/components/DynamicBanners.jsx
@@ -5,10 +5,12 @@ import { useQuery } from '@apollo/client'
 import Stack from '@mui/material/Stack'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
+import Fab from '@mui/material/Fab'
 import { styled } from '@mui/material/styles'
 import { DomEvent } from 'leaflet'
 import { CUSTOM_BANNERS } from '@services/queries/config'
 import { I } from '@components/I'
+import { useMemory } from '@store/useMemory'
 
 const StyledBannerStack = styled(Stack)(({ theme }) => ({
   position: 'fixed',
@@ -23,37 +25,36 @@ const StyledBannerStack = styled(Stack)(({ theme }) => ({
   },
 }))
 
-const BannerContainer = styled(Box, {
-  shouldForwardProp: (prop) => prop !== 'expanded' && prop !== 'backgroundColor',
-})(({ theme, expanded, backgroundColor }) => ({
+const BannerWrapper = styled(Box)({
+  position: 'relative',
   display: 'flex',
   alignItems: 'center',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: backgroundColor || theme.palette.background.paper,
-  boxShadow: theme.shadows[4],
-  overflow: 'hidden',
-  maxWidth: expanded ? 350 : 56,
-  width: expanded ? 'auto' : 56,
-  transition: theme.transitions.create(['maxWidth', 'width'], {
-    duration: theme.transitions.duration.enteringScreen,
-    easing: theme.transitions.easing.easeOut,
-  }),
-  [theme.breakpoints.down('sm')]: {
-    maxWidth: expanded ? 280 : 48,
-    width: expanded ? 'auto' : 48,
-  },
-}))
+  justifyContent: 'flex-end',
+})
 
-const IconContainer = styled(Box)(({ theme }) => ({
+const BannerPanel = styled(Box, {
+  shouldForwardProp: (prop) => prop !== 'expanded' && prop !== 'backgroundColor' && prop !== 'fabHeight',
+})(({ theme, expanded, backgroundColor, fabHeight }) => ({
+  position: 'absolute',
+  right: 0,
   display: 'flex',
   alignItems: 'center',
-  justifyContent: 'center',
-  minWidth: 56,
-  height: 56,
-  flexShrink: 0,
+  backgroundColor: backgroundColor || theme.palette.background.paper,
+  borderRadius: theme.shape.borderRadius,
+  boxShadow: theme.shadows[4],
+  paddingRight: theme.spacing(1),
+  height: fabHeight,
+  maxWidth: expanded ? 350 : 0,
+  width: expanded ? 'auto' : 0,
+  opacity: expanded ? 1 : 0,
+  overflow: 'hidden',
+  transition: theme.transitions.create(['maxWidth', 'width', 'opacity'], {
+    duration: 400,
+    easing: theme.transitions.easing.easeInOut,
+  }),
+  pointerEvents: expanded ? 'auto' : 'none',
   [theme.breakpoints.down('sm')]: {
-    minWidth: 48,
-    height: 48,
+    maxWidth: expanded ? 280 : 0,
   },
 }))
 
@@ -64,6 +65,7 @@ const ContentContainer = styled(Box)(({ theme }) => ({
   minWidth: 0,
   flex: 1,
   overflow: 'hidden',
+  justifyContent: 'center',
 }))
 
 const TitleText = styled(Typography)(({ theme }) => ({
@@ -98,9 +100,12 @@ const DescriptionText = styled(Typography)(({ theme }) => ({
  *     displayTime: number
  *     hideTime: number
  *   }
+ *   fabSize: 'small' | 'large'
+ *   iconSize: 'small' | 'medium'
+ *   fabHeight: number
  * }} props
  */
-function BannerItem({ banner }) {
+function BannerItem({ banner, fabSize, iconSize, fabHeight }) {
   const [isExpanded, setIsExpanded] = React.useState(false)
   const timersRef = React.useRef([])
 
@@ -145,68 +150,74 @@ function BannerItem({ banner }) {
   const isImageIcon = banner.icon?.startsWith('http')
 
   return (
-    <BannerContainer
-      expanded={isExpanded}
-      backgroundColor={banner.backgroundColor}
-      onClick={handleClick}
-      sx={{
-        cursor: banner.href ? 'pointer' : 'default',
-        '&:hover': banner.href
-          ? {
-              boxShadow: (theme) => theme.shadows[8],
-            }
-          : {},
-      }}
-    >
-      <IconContainer>
+    <BannerWrapper>
+      <BannerPanel
+        expanded={isExpanded}
+        backgroundColor={banner.backgroundColor}
+        fabHeight={fabHeight}
+        onClick={handleClick}
+        sx={{
+          cursor: banner.href ? 'pointer' : 'default',
+          '&:hover': banner.href
+            ? {
+                boxShadow: (theme) => theme.shadows[8],
+              }
+            : {},
+        }}
+      >
+        <ContentContainer>
+          <TitleText
+            variant="body2"
+            sx={{
+              color: banner.textColor || 'inherit',
+            }}
+          >
+            {banner.title}
+          </TitleText>
+          <DescriptionText
+            variant="body2"
+            component="div"
+            sx={{
+              color: banner.textColor || 'inherit',
+            }}
+            dangerouslySetInnerHTML={{ __html: banner.description || '' }}
+          />
+        </ContentContainer>
+      </BannerPanel>
+      <Fab
+        size={fabSize}
+        color="secondary"
+        onClick={handleClick}
+        sx={{
+          position: 'relative',
+          zIndex: 1,
+          backgroundColor: banner.backgroundColor || undefined,
+          color: banner.textColor || undefined,
+          '&:hover': {
+            backgroundColor: banner.backgroundColor || undefined,
+            boxShadow: (theme) => theme.shadows[8],
+          },
+        }}
+      >
         {isImageIcon ? (
           <img
             src={banner.icon}
             alt={banner.title}
             style={{
-              width: 24,
-              height: 24,
+              width: iconSize === 'small' ? 20 : 24,
+              height: iconSize === 'small' ? 20 : 24,
               objectFit: 'contain',
             }}
           />
         ) : (
           <I
             className={banner.icon}
-            size="medium"
-            color={banner.textColor || 'inherit'}
+            size={iconSize}
+            color={banner.textColor || 'white'}
           />
         )}
-      </IconContainer>
-      <ContentContainer
-        sx={{
-          opacity: isExpanded ? 1 : 0,
-          width: isExpanded ? 'auto' : 0,
-          transition: (theme) =>
-            theme.transitions.create(['opacity', 'width'], {
-              duration: theme.transitions.duration.enteringScreen,
-              easing: theme.transitions.easing.easeOut,
-            }),
-          pointerEvents: isExpanded ? 'auto' : 'none',
-        }}
-      >
-        <TitleText
-          variant="body2"
-          sx={{
-            color: banner.textColor || 'inherit',
-          }}
-        >
-          {banner.title}
-        </TitleText>
-        <DescriptionText
-          variant="body2"
-          component="div"
-          sx={{
-            color: banner.textColor || 'inherit',
-          }}
-          dangerouslySetInnerHTML={{ __html: banner.description || '' }}
-        />
-      </ContentContainer>
-    </BannerContainer>
+      </Fab>
+    </BannerWrapper>
   )
 }
 
@@ -215,11 +226,17 @@ export function DynamicBanners() {
     fetchPolicy: 'cache-first',
   })
   const ref = React.useRef(null)
+  const isMobile = useMemory((s) => s.isMobile)
 
   const banners = React.useMemo(
     () => (Array.isArray(data?.customBanners) ? data.customBanners : []),
     [data?.customBanners],
   )
+
+  const fabSize = isMobile ? 'small' : 'large'
+  const iconSize = isMobile ? 'small' : 'medium'
+  // Fab heights: small = 40px, large = 56px
+  const fabHeight = isMobile ? 40 : 56
 
   React.useEffect(() => {
     if (ref.current) {
@@ -234,7 +251,13 @@ export function DynamicBanners() {
   return (
     <StyledBannerStack ref={ref} direction="column">
       {banners.map((banner, index) => (
-        <BannerItem key={`${banner.icon}-${banner.href}-${index}`} banner={banner} />
+        <BannerItem
+          key={`${banner.icon}-${banner.href}-${index}`}
+          banner={banner}
+          fabSize={fabSize}
+          iconSize={iconSize}
+          fabHeight={fabHeight}
+        />
       ))}
     </StyledBannerStack>
   )
